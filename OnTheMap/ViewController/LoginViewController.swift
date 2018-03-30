fb4f1f137361271a0d3059a7ac15baafd356eaaa
Jason Hoopes <hoopes.jason@gmail.com>
2018-03-30 18:28:08 -0400

    Incremental update

--- OnTheMap/ViewController/LoginViewController.swift
+++ OnTheMap/ViewController/LoginViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  LoginViewController.swift
 //  OnTheMap
 //
 //  Created by Jason Hoopes on 3/29/18.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class LoginViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
